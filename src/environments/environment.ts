9a303344485033f6ffaa2ccf2653e7655d5ae867
Jiuzhou <Jiuzhou@rideshark.com>
2018-01-08 12:55:03 -0500

    BashChat Copied

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAFwTgC60C7Z20x9migd6dz8V8LgjwJ8jc',
+    authDomain: 'bashchat-b6206.firebaseapp.com',
+    databaseURL: 'https://bashchat-b6206.firebaseio.com',
+    projectId: 'bashchat-b6206',
+    storageBucket: '',
+    messagingSenderId: '932269319349'
+  }
+};
